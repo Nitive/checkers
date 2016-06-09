f62bcadf2a10588bdbabeecee3851dfb5ff705ea
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-06-09 11:06:57 +0700

    Add Coords type

--- src/Core.elm
+++ src/Core.elm
@@ -13,19 +13,25 @@ type alias Row = List Cell
 
 type alias Field = List Row
 
+type alias Coords =
+  { x : Int
+  , y : Int
+  }
 
-coordsToCell : Int -> Int -> Cell
-coordsToCell x y =
+
+
+coordsToCell : Coords -> Cell
+coordsToCell {x, y} =
   let
     color =
       if (x + y) % 2 == 0
         then White
         else Black
 
-    isBlack = color == Black
+    isBlackCell = color == Black
 
     cherker =
-      if isBlack
+      if isBlackCell
        then Color Black
        else None
 
@@ -36,5 +42,7 @@ coordsToCell x y =
 fieldSize = 8
 
 initialField =
-  List.indexedMap (coordsToCell >> List.map) <| List.repeat fieldSize [0..fieldSize-1]
+  List.indexedMap
+    (\x -> List.map (coordsToCell << Coords x))
+    (List.repeat fieldSize [0..fieldSize-1])
 
